fe3d13451ff5f16a716042c14da8aea98b0e42dd
kashishkumar-xyz <kashishkumar.xyz@gmail.com>
2025-10-17 20:52:41 +1100

    clean up - post horror merge

--- app/src/main/java/com/example/teeledger/models/BalanceSummary.kt
+++
@@ -1,8 +0,0 @@
-package com.example.teeledger.models
-
-import com.google.gson.annotations.SerializedName
-
-data class BalanceSummary(
-    @SerializedName("person") val personName: String,
-    @SerializedName("balance") val netBalance: Long
-)
